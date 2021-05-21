3c688e5e45e7b406003096f66b99668fc266c341
Aswebilius <81036333+Aswebilius@users.noreply.github.com>
2021-05-20 20:47:37 -0400

    created JS functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,25 @@
-var startButton = document.querySelector("#start-button");
+var startButtonEl = document.querySelector("#start-button");
+var buttonAEl = document.querySelector("#btn-a");
+var buttonBEl = document.querySelector("#btn-b");
+var buttonCEl = document.querySelector("#btn-c");
+var buttonDEl = document.querySelector("#btn-d");
+
+startButtonEl.addEventListener("click", function(){
+
+});
+
+buttonAEl.addEventListener("click", function(){
+    alert("button clicked");
+});
+
+buttonBEl.addEventListener("click", function(){
+    alert("button clicked");
+});
+
+buttonCEl.addEventListener("click", function(){
+    alert("button clicked");
+});
+
+buttonDEl.addEventListener("click", function(){
+    alert("button clicked");
+});
